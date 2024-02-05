6dc20c3a25a7c2f6ab441641c9f30b2406bb91a9
Canttellyou <akbar123jason@gmail.com>
2024-02-05 01:22:47 +0000

    fixes

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,20 +1,22 @@
 "use client";
-import React from "react";
+
+import { Fragment, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
+
 import { CustomFiltersProps } from "@/types";
 import { updateSearchParams } from "@/utils";
 
-const CustomFilter = ({ title, options }: CustomFiltersProps) => {
-  const [selected, setSelected] = useState(options[0]);
+export default function CustomFilter({ title, options }: CustomFiltersProps) {
   const router = useRouter();
+  const [selected, setSelected] = useState(options[0]); // State for storing the selected option
 
+  // update the URL search parameters and navigate to the new URL
   const handleUpdateParams = (e: { title: string; value: string }) => {
     const newPathName = updateSearchParams(title, e.value.toLowerCase());
 
-    router.push(newPathName, { scroll: false });
+    router.push(newPathName);
   };
 
   return (
@@ -22,46 +24,51 @@ const CustomFilter = ({ title, options }: CustomFiltersProps) => {
       <Listbox
         value={selected}
         onChange={(e) => {
-          setSelected(e);
-          handleUpdateParams(e);
+          setSelected(e); // Update the selected option in state
+          handleUpdateParams(e); // Update the URL search parameters and navigate to the new URL
         }}
       >
-        <div className="relative w-fit z-10 ">
-          <Listbox.Button className={"custom-filter__btn"}>
+        <div className="relative w-fit z-10">
+          {/* Button for the listbox */}
+          <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
             <Image
-              src={"/chevron-up-down.svg"}
+              src="/chevron-up-down.svg"
               width={20}
               height={20}
               className="ml-4 object-contain"
-              alt="chevron up down"
+              alt="chevron_up-down"
             />
           </Listbox.Button>
+          {/* Transition for displaying the options */}
           <Transition
-            as={Fragment}
+            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
             leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
             <Listbox.Options className="custom-filter__options">
+              {/* Map over the options and display them as listbox options */}
               {options.map((option) => (
                 <Listbox.Option
-                  value={option}
                   key={option.title}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 px-4 ${
-                      active ? "bg-primary-blue  text-white" : "text-gray-900"
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
                     }`
                   }
+                  value={option}
                 >
                   {({ selected }) => (
-                    <span
-                      className={`block truncate ${
-                        selected ? "font-medium" : "font-normal"
-                      } `}
-                    >
-                      {option.title}
-                    </span>
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {option.title}
+                      </span>
+                    </>
                   )}
                 </Listbox.Option>
               ))}
@@ -71,6 +78,4 @@ const CustomFilter = ({ title, options }: CustomFiltersProps) => {
       </Listbox>
     </div>
   );
-};
-
-export default CustomFilter;
+}
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,15 +1,16 @@
 "use client";
-import { useState } from "react";
-import { SearchManufacturer } from ".";
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+
+import { SearchManufacturer } from ".";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
     <Image
       src={"/magnifying-glass.svg"}
-      alt="magnifying glass"
+      alt={"magnifying glass"}
       width={40}
       height={40}
       className="object-contain"
@@ -18,40 +19,45 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 );
 
 const SearchBar = () => {
-  const [manufacturer, setManufacturer] = useState("");
+  const [manufacturer, setManuFacturer] = useState("");
   const [model, setModel] = useState("");
+
   const router = useRouter();
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (manufacturer === "" && model === "") {
-      return alert("Please select a manufacturer");
+    if (manufacturer.trim() === "" && model.trim() === "") {
+      return alert("Please provide some input");
     }
 
     updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
   };
 
   const updateSearchParams = (model: string, manufacturer: string) => {
+    // Create a new URLSearchParams object using the current URL search parameters
     const searchParams = new URLSearchParams(window.location.search);
 
+    // Update or delete the 'model' search parameter based on the 'model' value
     if (model) {
       searchParams.set("model", model);
     } else {
       searchParams.delete("model");
     }
 
+    // Update or delete the 'manufacturer' search parameter based on the 'manufacturer' value
     if (manufacturer) {
       searchParams.set("manufacturer", manufacturer);
     } else {
       searchParams.delete("manufacturer");
     }
 
+    // Generate the new pathname with the updated search parameters
     const newPathname = `${
       window.location.pathname
     }?${searchParams.toString()}`;
 
-    router.push(newPathname, { scroll: false });
+    router.push(newPathname);
   };
 
   return (
@@ -59,13 +65,13 @@ const SearchBar = () => {
       <div className="searchbar__item">
         <SearchManufacturer
           manufacturer={manufacturer}
-          setManufacturer={setManufacturer}
+          setManufacturer={setManuFacturer}
         />
         <SearchButton otherClasses="sm:hidden" />
       </div>
       <div className="searchbar__item">
         <Image
-          src={"/model-icon.png"}
+          src="/model-icon.png"
           width={25}
           height={25}
           className="absolute w-[20px] h-[20px] ml-4"
