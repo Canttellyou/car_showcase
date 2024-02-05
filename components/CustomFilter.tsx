dd3b984ad936055a0ae669e867304e19114cc557
Canttellyou <akbar123jason@gmail.com>
2024-02-05 10:30:02 +0000

    preventing scroll to top

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -16,7 +16,7 @@ export default function CustomFilter({ title, options }: CustomFiltersProps) {
   const handleUpdateParams = (e: { title: string; value: string }) => {
     const newPathName = updateSearchParams(title, e.value.toLowerCase());
 
-    router.push(newPathName);
+    router.push(newPathName, { scroll: false });
   };
 
   return (
